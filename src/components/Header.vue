f0742f7d5ff4a92fa4a00a19f404fda0e4672952
Rabel Obispo <rabelobispo@hotmail.com>
2020-02-03 21:18:56 -0500

    Header ready

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,43 @@
 <template>
-  <div>
-    <h1>Header</h1>
-  </div>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <router-link to="/porfolio" tag="li" activeClass="active"
+            ><a>Porfolio</a>
+          </router-link>
+          <router-link to="/stocks" tag="li" activeClass="active"
+            ><a>Stocks</a>
+          </router-link>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">End day</a></li>
+          <li class="dropdown">
+            <a
+              href="#"
+              class="dropdown-toggle"
+              data-toggle="dropdown"
+              role="button"
+              aria-haspopup="true"
+              aria-expanded="false"
+              >Save & Load <span class="caret"></span
+            ></a>
+            <ul class="dropdown-menu">
+              <li><a href="#">Save Data</a></li>
+              <li><a href="#">Load Data</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container-fluid -->
+  </nav>
 </template>
 
 <script>
