e0ebb801db161bf33066e2f39ede1956511475b4
Rabel Obispo <rabelobispo@hotmail.com>
2020-02-05 20:57:13 -0500

    Stocks

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,11 +1,28 @@
 <template>
   <div>
-    <h1>Stocks</h1>
+    <app-stock v-for="stock in stocks" :key="stock.id" :stock="stock"></app-stock>
   </div>
 </template>
 
 <script>
-  export default {};
+  import Stock from './Stock.vue';
+
+  export default {
+    components: {
+      appStock: Stock
+    },
+    data() {
+      return {
+        stocks: [
+          { id: 1, name: 'Apple', price: 150 },
+          { id: 2, name: 'Amazon', price: 140 },
+          { id: 3, name: 'Microsoft', price: 130 },
+          { id: 4, name: 'Facebook', price: 120 },
+          { id: 5, name: 'WhatsApp', price: 100 }
+        ]
+      };
+    }
+  };
 </script>
 
 <style scoped></style>
